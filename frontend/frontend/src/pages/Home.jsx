6a282308e8249bf3d3ecbe1826cbdb32abd6ab2c
nikhil-m-star <nikhilm9110@gmail.com>
2025-09-30 20:33:50 +0530

    home

--- frontend/frontend/src/pages/Home.jsx
+++ frontend/frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
         <div className="page home">
             <div className="glass">
                 <h1>Welcome to Lost and Found</h1>
-                <p>Use the navigation to report found or lost items.</p>
+                
             </div>
         </div>
     );
